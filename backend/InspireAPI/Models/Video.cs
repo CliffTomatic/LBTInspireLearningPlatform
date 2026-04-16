3d4dd3a5e5f1abae15181310833b51cd28f3a127
Clifford Durbin <clifforddurbin@gmail.com>
2026-04-15 21:26:30 -0700

    Generated dotnet project starter code and added extra folders/files to the architecture like "Model" and "Services."

---
+++ backend/InspireAPI/Models/Video.cs
@@ -0,0 +1,10 @@
+// Objects for the app to use.
+public class Video
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string ThumbnailUrl { get; set; } = "";
+    public string VideoUrl { get; set; } = "";
+    public int DurationSeconds { get; set; }
+}
---
+++ backend/InspireAPI/Services/SessionService.cs
@@ -0,0 +1,1 @@
+// Helper Logic/Functions. Mainly for Controllers.
