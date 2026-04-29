fe1bb302e77fadb84b8d541077c58e3b9072b32f
Clifford Durbin <clifforddurbin@gmail.com>
2026-04-28 22:27:28 -0700

    Add page skeleton

--- frontend/src/pages/LearnPage/LearnPage.tsx
+++ frontend/src/pages/LearnPage/LearnPage.tsx
@@ -1,4 +1,31 @@
+import { useParams } from "react-router-dom";
+import "./LearnPage.css";
+
 function LearnPage() {
-    return (<h1>LearnPage</h1>);
+    const { courseSlug } = useParams();
+
+    return (
+        <section className="learn-page">
+            <div className="learn-page__content page-container">
+                <div className="learn-page__header">
+                    <p className="learn-page__eyebrow">Now learning</p>
+                    <h1 className="learn-page__title">{courseSlug}</h1>
+                </div>
+
+                <div className="learn-page__layout">
+                    <aside className="learn-page__sidebar">
+                        Lesson Sidebar
+                    </aside>
+
+                    <div className="learn-page__main">
+                        <div className="learn-page__video-placeholder">
+                            Video Player
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
+
 export default LearnPage;
