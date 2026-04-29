d803ccbf727fcacddf56e355915c62d75ef45363
Clifford Durbin <clifforddurbin@gmail.com>
2026-04-28 21:58:36 -0700

    Created Footer Component

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="page-container">
+                <p className="footer__company">LearnBasicTech</p>
+                <p className="footer__copyright">Copyright © 2026</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
