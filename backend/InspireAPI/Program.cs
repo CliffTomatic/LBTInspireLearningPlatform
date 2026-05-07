4665de4d0e78cdd5f3505e23980de7d9e26a438e
Clifford Durbin <clifforddurbin@gmail.com>
2026-05-06 22:10:53 -0700

    Fix Swagger/OpenAPI package mismatch

--- backend/InspireAPI/Program.cs
+++ backend/InspireAPI/Program.cs
@@ -7,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSingleton<VideoService>();
 
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // SQLite
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? "Data Source=MockServer/MockData/pulse.db";
@@ -16,7 +20,19 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
